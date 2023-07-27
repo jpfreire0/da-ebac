d1ca27e417808f73217725698b3ed712e0bd3215
jpfreire0 <jpfreiremessias@gmail.com>
2023-07-27 17:13:26 +0000

    atualizando arquivos

--- gasolina.py
+++ gasolina.py
@@ -7,5 +7,5 @@ df = pd.read_csv('gasolina.csv')
 
 with sns.axes_style('whitegrid'):
   grafico = sns.lineplot(data=df, x="dia", y="venda", errorbar=None)
-  grafico.set(title='Valor de venda por dia', xlabel='Dias', ylabel='Valor')
+  grafico.set(title='Price per day', xlabel='Day', ylabel='Price')
 plt.savefig('gasolina.png')
